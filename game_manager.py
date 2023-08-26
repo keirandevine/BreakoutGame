9aa991a78728a3ea34bd7a8b6fa8aae45ab9646c
Keiran <teacherkeiran@gmail.com>
2023-08-26 08:12:51 +0100

    Remake paddle and collision detection. Display player lives. Set random heading for newly created ball. Set new angle of movement for ball based on collision with edge of paddle.

--- game_manager.py
+++ game_manager.py
@@ -1,48 +1,68 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
 
 class Paddle:
         def __init__(self):
-            self.screen = screen
-            self.paddle = []
-            self.head_turtle = Turtle()
+            self.paddle = Turtle()
+            self.xcor = 0
+            self.ycor = 0
+            self.left_end = Turtle()
+            self.left_end_y = self.ycor - 49
+            self.right_end_y = self.xcor + 49
+            self.right_end = Turtle()
+
 
         def create_paddle(self):
-            xcor = -50
+            """Creates a paddle and sets its position at the bottom of the screen"""
             screen.tracer(0)
-            for i in range(7):
-                segment = Turtle()
-                segment.penup()
-                segment.shape("square")
-                segment.color("white")
-                segment.shapesize(0.7)
-                segment.goto(xcor, -250)
-                segment.setheading(0)
-                segment.showturtle()
-                xcor += 15
-                self.paddle.append(segment)
-            self.head_turtle = self.paddle[0]
-            self.head_turtle.color("blue")
-            self.paddle[6].color("blue")
+            paddle = Turtle()
+            paddle.penup()
+            paddle.shape("square")
+            paddle.color("white")
+            paddle.shapesize(0.6, stretch_len=5)
+            paddle.goto(0, -260)
+            paddle.setheading(0)
+            left_end = Turtle()
+            left_end.penup()
+            left_end.color("blue")
+            left_end.shape("circle")
+            left_end.shapesize(0.6)
+            left_end.goto(paddle.xcor() - 49, -260)
+            right_end = Turtle()
+            right_end.penup()
+            right_end.color("blue")
+            right_end.shape("circle")
+            right_end.shapesize(0.6)
+            right_end.goto(paddle.xcor() + 49, -260)
+            self.paddle = paddle
+            self.left_end = left_end
+            self.right_end = right_end
             screen.update()
 
         def move_left(self):
-            self.head_turtle.backward(50)
-            xcor = self.head_turtle.xcor() + 15
-            for paddle in self.paddle:
-                paddle.goto(xcor, -250)
-                xcor += 15
-                self.screen.update()
+            """Moves paddle 30 pixels to the left"""
+            screen.tracer(0)
+            self.paddle.backward(30)
+            self.left_end.backward(30)
+            self.right_end.backward(30)
+            screen.update()
+
 
         def move_right(self):
-            self.head_turtle.forward(50)
-            xcor = self.head_turtle.xcor() + 15
-            for paddle in self.paddle:
-                paddle.goto(xcor, -250)
-                xcor += 15
-                self.screen.update()
+            """Moves paddle 30 pixels to the right"""
+            screen.tracer(0)
+            self.paddle.forward(30)
+            self.left_end.forward(30)
+            self.right_end.forward(30)
+            screen.update()
+
+        def check_coordinates(self):
+            """Updates the xcor and ycor attributes to the current turtle position"""
+            self.xcor = self.paddle.xcor()
+            self.ycor = self.paddle.ycor()
 
 class Ball:
 
@@ -50,51 +70,40 @@ class Ball:
             self.ball = Turtle()
             self.screen = screen
             self.paddle = paddle
-            self.speed = 1
+            self.speed = 0.5
             self.xcor = 0
             self.ycor = 0
 
-        def create_ball(self):
+        def create_ball(self, heading):
+            """Takes a heading as an input, creates a ball and sets it's heading to the one input"""
             self.ball.goto(0, -120)
             self.ball.shape("circle")
             self.ball.shapesize(0.8)
             self.ball.penup()
             self.ball.color("white")
             self.ball.showturtle()
-            self.ball.setheading(240)
+            self.ball.setheading(heading)
             self.xcor = self.ball.xcor()
             self.ycor = self.ball.ycor()
 
 
         def move(self):
+            """Moves the ball forward by the amount defined in the class speed attribute"""
             self.ball.forward(self.speed)
             self.screen.update()
 
         def bounce_hor(self):
+            """Resets the ball heading to reflect a bounce off a horizontal surface"""
             heading = 360 - self.ball.heading()
             self.ball.setheading(heading)
 
         def bounce_ver(self):
+            """Resets the ball heading to reflect a bounce off a vertical surface"""
             heading = 180 - self.ball.heading()
             self.ball.setheading(heading)
 
-        def hit_paddle(self):
-            for segment in self.paddle:
-                if self.ball.distance(segment.xcor(), segment.ycor()) <= 16:
-                    self.bounce_hor()
-            if self.ball.distance(self.paddle[0].xcor(), self.paddle[0].ycor()) <= 10:
-                self.ball.setheading(self.ball.heading() + 20)
-            if self.ball.distance(self.paddle[6].xcor(), self.paddle[6].ycor()) <= 10:
-                self.ball.setheading(self.ball.heading() - 20)
-
-        def hit_block(self, block_list, brain):
-            for block in block_list:
-                if self.ball.distance(block.xcor, block.ycor) < 20 and self.ball.ycor() <= block.ycor - 6:
-                    self.bounce_hor()
-                    brain.break_block(block_list.index(block))
-
-
         def hit_walls(self):
+            """Detects collissions with the north, west and east walls then calls the appropriate type of bounce"""
             if self.ball.xcor() >= 178 or self.ball.xcor() <= -185:
                 self.bounce_ver()
             if self.ball.ycor() >= 225:
@@ -102,9 +111,20 @@ class Ball:
 
 
         def check_position(self):
+            """Updates the class attributes xcor and ycor to current turtle position"""
             self.xcor = self.ball.xcor()
             self.ycor = self.ball.ycor()
 
+
+        def change_angle_left(self):
+            """Changes the angle of the ball to reflect a collision with the left of the paddle"""
+            heading = self.ball.heading()
+            self.ball.setheading(heading + 15)
+
+        def change_angle_right(self):
+            """Changes the angle of the ball to reflect a collision with the right of the paddle"""
+            heading = self.ball.heading()
+            self.ball.setheading(heading - 15)
 
 class Block:
     def __init__(self):
@@ -114,7 +134,9 @@ class Block:
         self.score = 20
 
 
+
     def make_block(self):
+        """Creates a new block and sets its default colour to yellow"""
         block = Turtle()
         block.color("yellow")
         block.shape("square")
@@ -131,9 +153,15 @@ class GameBrain:
         self.blocks_list = []
         self.score = 0
         self.level = 1
+        self.lives = 3
+        self.level_pen = Turtle()
+        self.score_pen = Turtle()
+        self.lives_pen = Turtle()
         self.pen = Turtle()
 
+
     def top_line(self):
+        """Draws the boundary at the north of the screen"""
         pen = Turtle()
         pen.penup()
         pen.pensize(3)
@@ -143,6 +171,7 @@ class GameBrain:
         pen.goto(200, 230)
 
     def create_scoreboard(self):
+        """Draws the scoreboard in the north-west corner of the screen"""
         self.pen.penup()
         self.pen.hideturtle()
         self.pen.pencolor("white")
@@ -151,6 +180,7 @@ class GameBrain:
 
 
     def update_score(self):
+        """Clears and redraws the scoreboard to reflect updated score"""
         self.pen.clear()
         self.pen.hideturtle()
         self.pen.pencolor("white")
@@ -159,18 +189,31 @@ class GameBrain:
 
 
     def display_level(self, level):
-        pen = Turtle()
-        pen.penup()
-        pen.hideturtle()
-        pen.goto(60, 250)
-        pen.color("white")
-        pen.write(f"Level: {level}", font=("Arial", 20, "normal"))
+        """Draws the level display in the north-east of the screen"""
+        self.level_pen.clear()
+        self.level_pen.penup()
+        self.level_pen.hideturtle()
+        self.level_pen.goto(60, 250)
+        self.level_pen.color("white")
+        self.level_pen.write(f"Level: {level}", font=("Arial", 20, "normal"))
 
-    def check_collision(self, ball, block):
+
+    def display_lives(self, lives):
+        """Draws the lives display in the north-east of the screen"""
+        self.lives_pen.clear()
+        self.lives_pen.penup()
+        self.lives_pen.hideturtle()
+        self.lives_pen.goto(-180, 230)
+        self.lives_pen.color("white")
+        self.lives_pen.write(f"Lives: {lives}", font=("Arial", 10, "normal"))
+
+
+    def check_block_collision(self, ball, block):
+        """Returns true if ball and block come into contact, otherwise returns false"""
         x1, y1 = ball.xcor, ball.ycor
         x2, y2 = block.xcor, block.ycor
         width1, height1 = 14, 14
-        width2, height2 = 18, 36
+        width2, height2 = 36, 18
 
         if (x1 + width1 / 2 > x2 - width2 / 2 and
                 x1 - width1 / 2 < x2 + width2 / 2 and
@@ -178,6 +221,62 @@ class GameBrain:
                 y1 - height1 / 2 < y2 + height2 / 2):
             return True
         return False
+
+    def check_paddle_collision(self, ball, paddle):
+        """Returns true if ball comes into contact with paddle, otherwise returns false"""
+        x1, y1 = ball.xcor, ball.ycor
+        x2, y2 = paddle.xcor, paddle.ycor
+        width1, height1 = 14, 14
+        width2, height2 = 90, 12
+
+        if (x1 + width1 / 2 > x2 - width2 / 2 and
+                x1 - width1 / 2 < x2 + width2 / 2 and
+                y1 + height1 / 2 > y2 - height2 / 2 and
+                y1 - height1 / 2 < y2 + height2 / 2):
+            return True
+        return False
+
+    def check_left_pad_collision(self, ball, paddle):
+        """Returns true if ball comes into contact with left edge of paddle, otherwise returns false"""
+        if ball.distance(paddle.xcor - 49, -265) < 22:
+            return True
+        return False
+
+    def check_right_pad_collision(self, ball, paddle):
+        """Returns true if ball comes into contact with right edge of paddle, otherwise returns false"""
+        if ball.distance(paddle.xcor + 49, -265) < 22:
+            return True
+        return False
+
+    def random_heading(self):
+        """Creates a new random heading to be used as an input upon creation of a new ball"""
+        random_heading1 = random.randint(240, 260)
+        random_heading2 = random.randint(280, 300)
+        headings = [random_heading1, random_heading2]
+        random_heading = random.choice(headings)
+        return random_heading
+
+    def game_over_msg(self, score):
+        """Displays a game over message along with player's score"""
+        pen = Turtle()
+        pen.penup()
+        pen.pencolor("white")
+        pen.goto(0, -100)
+        pen.write(f"Game over\nFinal Score: {score}", font=("Arial", 20, "normal"), align="center")
+
+
+    def declare_winner(self, score):
+        """Displays a winner message along with player's score"""
+        pen = Turtle()
+        pen.penup()
+        pen.pencolor("white")
+        pen.goto(0, -100)
+        pen.write(f"Congratulations. You win!\nFinal Score: {score}", font=("Arial", 20, "normal"), align="center")
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from game_manager import Paddle, screen, Ball, GameBrain, Block
 #________________________________________Definition of Functions_______________________________________________#
 
 def set_up_level():
-    ball.create_ball()
+    """Creates a new ball, blocks, scoreboard and level display. Draws the line at the top of the screen"""
+    ball.create_ball(brain.random_heading())
     lay_blocks(brain.level)
     brain.top_line()
     brain.create_scoreboard()
@@ -14,10 +15,12 @@ def set_up_level():
 
 
 def lay_blocks(level):
+    """Creates the appropriate number of blocks for the level. Lays out blocks in correct position and changes block
+    color and strength to reflect level"""
     xcor = -162
-    ycor = 50
+    ycor = 0
     global brain
-    for i in range((level + 6) * 8):
+    for i in range((level) * 8):
         new_block = block.make_block()
         new_block.score = 20
         new_block.goto(xcor, ycor)
@@ -26,6 +29,9 @@ def lay_blocks(level):
         new_block.ycor = ycor
         new_block.strength = 1
         xcor += 45
+
+        #Check position of blocks and power up
+
         if i > 15:
             new_block.strength += 1
             new_block.score += 20
@@ -38,7 +44,7 @@ def lay_blocks(level):
                     new_block.strength += 1
                     new_block.score += 20
                     new_block.color("red")
-        if xcor >= 162: ##Once blocks reach the edge of the screen, reset xcor and increase ycor to lay next row
+        if xcor >= 162:
             xcor = -162
             ycor += 12
 
@@ -83,12 +89,21 @@ game_over = False
 while not game_over:
     if brain.blocks_list != []:
         ball.move()
-        ball.hit_paddle()
         ball.hit_walls()
         ball.check_position()
+        paddle.check_coordinates()
         brain.update_score()
+        brain.display_lives(brain.lives)
+        if brain.check_paddle_collision(ball, paddle):
+            ball.bounce_hor()
+            if brain.check_left_pad_collision(ball.ball, paddle):
+                print("Hit left paddle")
+                ball.change_angle_left()
+            if brain.check_right_pad_collision(ball.ball, paddle):
+                print("Hit right paddle")
+                ball.change_angle_right()
         for block in brain.blocks_list:
-            if brain.check_collision(ball, block):
+            if brain.check_block_collision(ball, block):
                 ball.bounce_hor()
                 brain.score += block.score
                 brain.update_score()
@@ -96,11 +111,26 @@ while not game_over:
                 if block.strength == 0:
                     brain.blocks_list.remove(block)
                     block.hideturtle()
+        if ball.ycor <= -265:
+            brain.lives -= 1
+            ball.create_ball(brain.random_heading())
+        if brain.lives == 0:
+            game_over = True
+            brain.game_over_msg(score=brain.score)
 
 
     else:
+        block = Block()
         set_up_level()
         brain.level += 1
+        if brain.level == 8:
+            game_over = True
+
+
+#________________________________________Breakout of Game Loop_______________________________________________#
+
+
+brain.declare_winner(brain.score())
 
 
 
